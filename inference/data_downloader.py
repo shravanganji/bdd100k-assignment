7dba8a788f4b46d47e7570f016139c1494e1b1cb
shravanganji <58696076+shravanganji@users.noreply.github.com>
2024-02-18 01:53:33 +0530

    Update data_downloader.py

--- inference/data_downloader.py
+++ inference/data_downloader.py
@@ -4,7 +4,7 @@ import os
 
 
 
-url = 'https://drive.google.com/uc?id=1ovE01IRNoVbEnqX7q7uW66Am9G5NiKYv'
+url = 'https://drive.google.com/uc?id=1O4GYEXtIZrvfHkOmPKzIcmg6A2X2DmOf'
 output = 'data/bdd100k.zip'
 gdown.download(url, output, quiet=False)
 
