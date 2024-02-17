d1e1855bbe56b68f847260f1995f6aafddfc8417
shravanganji <58696076+shravanganji@users.noreply.github.com>
2024-02-17 19:30:20 +0530

    Update data_downloader.py

--- data_analysis/data_downloader.py
+++ data_analysis/data_downloader.py
@@ -1,15 +1,7 @@
 import gdown
 
-import os
-
-# get the current working directory
-current_working_directory = os.getcwd()
-
-# print output to the console
-print(current_working_directory)
-
-#url = 'https://drive.google.com/uc?id=1NgWX5YfEKbloAKX9l8kUVJFpWFlUO8UT'
-#output = 'data_final/bdd100k.tgz'
-#gdown.download(url, output, quiet=False)
+url = 'https://drive.google.com/uc?id=1WS-Pqje2d42mjxj6WJuVrrUtsrj6R9qE'
+output = 'csv_data.zip'
+gdown.download(url, output, quiet=False)
 
 
